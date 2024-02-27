69919e1dfd5419ce2818d52e266efcf196856054
Priyanka <priyanka08993@gmail.com>
2024-02-28 00:06:23 +0530

    Modified step and reward function

--- Environment.py
+++ Environment.py
@@ -15,8 +15,8 @@ class DemandForecastingEnv(gym.Env):
         self.current_step = 0
 
         # Define action and observation spaces
-        self.action_space = gym.spaces.Box(low=10.5, high=40.0 , shape=(14,), dtype=np.float32)
-        self.observation_space = gym.spaces.Box(low=10.0, high=50.0, shape=(84,)) 
+        self.action_space = gym.spaces.Box(low=10.0 , high=55.0 , shape=(14,), dtype=np.float32)
+        self.observation_space = gym.spaces.Box(low=15, high=50, shape=(84,)) 
 
     def reset(self):
         self.current_step = 0
@@ -30,18 +30,12 @@ class DemandForecastingEnv(gym.Env):
         # Calculate reward
         reward = self.calculate_reward(action, next_state)
         # Check if the episode is done (end of data)
-        done = self.current_step >= len(self.x_data) - self.lookback_window
+        print("Current Step:::",self.current_step)
+        done = self.current_step >= (len(self.x_data)-1)
         print("action is :::", action)
         print("reward is :::", reward)
         print("state is :::", next_state)
         return next_state, reward, done, {}
-        # elif self.current_step == 0:
-        #     next_state = self.x_data[self.current_step]
-        #     # Calculate reward
-        #     reward = self.calculate_reward(action, next_state)
-        #     # Check if the episode is done (end of data)
-        #     done = self.current_step >= len(self.x_data) - self.lookback_window
-        #     return next_state, reward, done, {}
 
 
     def calculate_reward(self, action, observation):
--- Rewards.py
+++ Rewards.py
@@ -6,11 +6,16 @@ class RewardCalculator():
 
     def calculate_reward(self,true_val,predicted_val):          
         # Calculate the absolute difference between the true and the predicted values
-        difference = sum(abs(test - predicted) for test, predicted in zip(true_val, predicted_val))                                                                          
-        # Calculate the reward based on the difference
-        reward = 1.0 / (1.0 + difference)        
-        return reward
-        
+        absolute_difference = [abs(test - predicted) for test, predicted in zip(true_val, predicted_val)]
 
+        normalized_difference = (absolute_difference - np.min(absolute_difference)) / (np.max(absolute_difference) - np.min(absolute_difference))
+        
+        # Map the normalized difference to the desired reward range [10, 50] using a sigmoid function
+        sigm_reward = 10 + normalized_difference
+
+        reward = np.mean(sigm_reward)
+
+        # Calculate the reward based on the difference
+        return reward
   
         
